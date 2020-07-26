7b66271a7960780aeaf0ef526eadf5a897cc4af7
David Lara <dave.lara21@gmail.com>
2020-07-26 17:27:41 -0500

    profile activity added

--- app/src/main/java/com/example/hci_project/SavedSearchActivity.java
+++ app/src/main/java/com/example/hci_project/SavedSearchActivity.java
@@ -2,7 +2,9 @@ package com.example.hci_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SavedSearchActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SavedSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_search);
+    }
+
+    public void mainActivitybuttonOnclick(View v){
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(i);
     }
 }
