123b8b23adc26058c158340e44feba89b7afa418
David Lara <dave.lara21@gmail.com>
2020-07-28 08:31:22 -0500

    defining search activity views

--- app/src/main/java/com/example/hci_project/SearchActivity.java
+++ app/src/main/java/com/example/hci_project/SearchActivity.java
@@ -1,21 +1,39 @@
 package com.example.hci_project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
-    SearchView sv;
-    ListView lv;
+    ListView listView;
+
+    //holds the charity names
+    String mTitle[] = {"charity1", "charity2","charity3"};
+
+    //holds the description information aka the charity tags (diseases)
+    String mDescription[] = {"charity1 description","charity2 description","charity3 description"};
+
+    //charity image
+    int images[] = {R.drawable.holdinghands, R.drawable.clouds, R.drawable.offwhite};
+
     ArrayList<String> list1;
     ArrayAdapter adapter;
 
@@ -24,27 +42,55 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        lv = (ListView)findViewById(R.id.list);
-        sv = (SearchView)findViewById(R.id.search);
-        list1 = new ArrayList<String>();
-        list1.add("one");
-        list1.add("two");
-        list1.add("three");
-        list1.add("four");
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,list1);
-        lv.setAdapter(adapter);
-        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
+        listView = findViewById(R.id.listView);
 
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-                return false;
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    Toast.makeText(SearchActivity.this, "facebook", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 1){
+                    Toast.makeText(SearchActivity.this, "facebook", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 2){
+                    Toast.makeText(SearchActivity.this, "facebook", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
 
+    class MyAdapter extends ArrayAdapter<String>{
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter (Context c, String title[], String description[], int imgs[]){
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImgs = imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+            return row;
+        }
     }
 }
